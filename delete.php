fa30b71a487b95ef07330d9d11e72fcda9273d7e
Rohit S Patil <127521022+rohitsp341@users.noreply.github.com>
2024-02-22 16:56:48 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,13 +4,12 @@ include("connection.php");
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Perform the delete query
     $deleteQuery = "DELETE FROM register_user3 WHERE id = $id";
 
     if (mysqli_query($conn, $deleteQuery)) {
         echo "<script>alert('Data Deleted successfully');</script>";
         echo "<script>window.location.href = 'index.php';</script>";
-        exit();  // Add exit() to stop further execution
+        exit();  // stop execution
     } else {
         echo "Error deleting record: " . mysqli_error($conn);
     }
@@ -18,7 +17,7 @@ if (isset($_GET['id'])) {
     echo "Invalid request";
 }
 
-// Redirect to the main page if the 'id' is not set
+// Redirect to the main page 
 
 header("Location: index.php");
 echo "<script>alert('Data Deleted successfully');</script>";
